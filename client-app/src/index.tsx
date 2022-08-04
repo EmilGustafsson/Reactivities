814c04fe4bb5717c4dcdf408458517b265bae599
Emil Gustafsson <emil.gustafsson@stamford.se>
2022-08-05 01:39:11 +0200

    End of section 11

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client';
 import 'react-calendar/dist/Calendar.css';
 import './app/layout/style.css';
 import 'react-toastify/dist/ReactToastify.min.css'
+import 'react-datepicker/dist/react-datepicker.css'
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
-import { BrowserRouter, Router } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 
 const root = ReactDOM.createRoot(
