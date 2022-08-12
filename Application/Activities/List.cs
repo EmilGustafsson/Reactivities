4981a498744e0776a8c88424df1cf72060c192db
Emil Gustafsson <emil@utvecklare.se>
2022-08-12 13:57:28 +0100

    End of section 20

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -9,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Application.Core;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using Application.Interfaces;
 
 namespace Application.Activities
 {
@@ -22,9 +23,11 @@ namespace Application.Activities
         {
             private readonly DataContext _context;
             private readonly IMapper _mapper;
+            private readonly IUserAccessor _userAccessor;
 
-            public Handler(DataContext context, IMapper mapper)
+            public Handler(DataContext context, IMapper mapper, IUserAccessor userAccessor)
             {
+            this._userAccessor = userAccessor;
                 _context = context;
                 _mapper = mapper;
             }
@@ -32,7 +35,8 @@ namespace Application.Activities
             public async Task<Result<List<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
             {
                 var activities = await _context.Activities
-                    .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider)
+                    .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider, 
+                                new { currentUsername = _userAccessor.GetUsername()})
                     .ToListAsync();
                 return Result<List<ActivityDto>>.Success(activities);
             }
